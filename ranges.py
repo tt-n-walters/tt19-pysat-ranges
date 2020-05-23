bdf420fec11d60f594692a231564f92f050a4332
nw <nico.walters@techtalents.es>
2020-05-23 17:03:05 +0200

    Added doc strings

--- ranges.py
+++ ranges.py
@@ -1,7 +1,11 @@
 
+# Analagous to builtins.range(), but accepts float values
+# stepped(5, 6, 0.2) -> [5, 5.2, 5.4, 5.6, 5.8]
 def stepped(start, stop, step):
     pass
 
 
+# A linear space with a given number of samples
+# linear(1, 2, 10) -> [1, 1.1, 1.2, 1.3, ..., 1.9]
 def linear(start, stop, samples):
     pass
